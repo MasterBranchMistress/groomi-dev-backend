9325aee7c9756a610de644834fdc492a05353dec
masterbranchmistress <masterbranchmistress@gmail.com>
2026-07-13 22:22:14 -0500

    Working base API - GROOMI-17 | First commit

---
+++ src/main/java/org/groomi/groomidevbackend/GroomiDevBackendApplication.java
@@ -0,0 +1,13 @@
+package org.groomi.groomidevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomiDevBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomiDevBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/groomi/groomidevbackend/GroomiDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.groomi.groomidevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomiDevBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
